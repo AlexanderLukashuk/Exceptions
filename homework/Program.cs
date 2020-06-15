22c7e0f61fbc0c7bd060fca82dcc49520ba06484
Саня <sanya@Sergejs-MacBook-Air.local>
2020-06-15 14:21:23 +0600

    homework

---
+++ homework/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Результат деления: {Division()}");
+
+        }
+
+        static int Division()
+        {
+            int result = 0;
+
+            int firstNumber, secondNumber;
+            string input;
+            Console.WriteLine("Enter first number: ");
+            input = Console.ReadLine();
+            firstNumber = int.Parse(input);
+
+            Console.WriteLine("Enter second number: ");
+            input = Console.ReadLine();
+            secondNumber = int.Parse(input);
+
+            /*if (secondNumber == 0)
+            {
+                throw new ArgumentException("Нельзя делить на ноль");
+            }*/
+
+            try
+            {
+                result = firstNumber / secondNumber;
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Результат будет равен 0");
+            }
+            catch (DivideByZeroException exception)
+            {
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Результат будет равен 0");
+            }
+            /*catch (ArgumentException exception) when (secondNumber == 0)
+            {
+                Console.WriteLine(exception.GetType().Name);
+                Console.WriteLine(exception.Message);
+                Console.WriteLine("Результат будет равен 0");
+            }*/
+
+            return result;
+        }
+    }
+}
---
+++ homework_2task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homework_2task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int SIZE_OF_ARRAY = 10;
+            int[] arrayOfNumbers = new int[SIZE_OF_ARRAY];
+            Random rand = new Random();
+
+            for (int i = 0; i < SIZE_OF_ARRAY; i++)
+            {
+                arrayOfNumbers[i] = rand.Next() % 20;
+            }
+
+            try
+            {
+                for (int i = 0; i <= SIZE_OF_ARRAY; i++)
+                {
+                    Console.Write($"{arrayOfNumbers[i]}");
+                    Console.Write(" ");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine();
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
